d77f7d3a0c26ca87e0990a3a621457b98eecda79
xzmzm <xingzhimeng@gmail.com>
2025-09-06 23:14:43 +0800

    Update UI.

--- src/OpenCvSharpEx.Sample/Form1.Designer.cs
+++ src/OpenCvSharpEx.Sample/Form1.Designer.cs
@@ -28,47 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnLoadPattern = new System.Windows.Forms.Button();
+            this.btnTeach = new System.Windows.Forms.Button();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(181, 48);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(305, 48);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // tableLayoutPanel1
+            //
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnLoadPattern, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnTeach, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnLoadImage, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSearch, 3, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(984, 40);
+            this.tableLayoutPanel1.TabIndex = 0;
+            //
+            // btnLoadPattern
+            //
+            this.btnLoadPattern.AutoSize = true;
+            this.btnLoadPattern.Location = new System.Drawing.Point(3, 3);
+            this.btnLoadPattern.Name = "btnLoadPattern";
+            this.btnLoadPattern.Size = new System.Drawing.Size(100, 30);
+            this.btnLoadPattern.TabIndex = 0;
+            this.btnLoadPattern.Text = "Load Pattern";
+            this.btnLoadPattern.UseVisualStyleBackColor = true;
+            this.btnLoadPattern.Click += new System.EventHandler(this.OnLoadPatternClick);
+            //
+            // btnTeach
+            //
+            this.btnTeach.AutoSize = true;
+            this.btnTeach.Location = new System.Drawing.Point(109, 3);
+            this.btnTeach.Name = "btnTeach";
+            this.btnTeach.Size = new System.Drawing.Size(100, 30);
+            this.btnTeach.TabIndex = 1;
+            this.btnTeach.Text = "Teach";
+            this.btnTeach.UseVisualStyleBackColor = true;
+            this.btnTeach.Click += new System.EventHandler(this.OnTeachClick);
+            //
+            // btnLoadImage
+            //
+            this.btnLoadImage.AutoSize = true;
+            this.btnLoadImage.Location = new System.Drawing.Point(215, 3);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(100, 30);
+            this.btnLoadImage.TabIndex = 2;
+            this.btnLoadImage.Text = "Load Image";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.OnLoadImageClick);
+            //
+            // btnSearch
+            //
+            this.btnSearch.AutoSize = true;
+            this.btnSearch.Location = new System.Drawing.Point(321, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 30);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.OnSearchClick);
+            //
+            // splitContainer1
+            //
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 40);
+            this.splitContainer1.Name = "splitContainer1";
+            //
+            // splitContainer1.Panel1
+            //
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            //
+            // splitContainer1.Panel2
+            //
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(984, 521);
+            this.splitContainer1.SplitterDistance = 328;
+            this.splitContainer1.TabIndex = 1;
+            //
+            // splitContainer2
+            //
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            //
+            // splitContainer2.Panel1
+            //
+            this.splitContainer2.Panel1.Controls.Add(this.propertyGrid1);
+            //
+            // splitContainer2.Panel2
+            //
+            this.splitContainer2.Panel2.Controls.Add(this.txtOutput);
+            this.splitContainer2.Size = new System.Drawing.Size(328, 521);
+            this.splitContainer2.SplitterDistance = 280;
+            this.splitContainer2.TabIndex = 0;
+            //
+            // propertyGrid1
+            //
+            this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertyGrid1.Location = new System.Drawing.Point(0, 0);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(328, 280);
+            this.propertyGrid1.TabIndex = 0;
+            this.propertyGrid1.PropertyValueChanged += new System.Windows.Forms.PropertyValueChangedEventHandler(this.OnPropertyValueChanged);
+            //
+            // txtOutput
+            //
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOutput.Location = new System.Drawing.Point(0, 0);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(328, 237);
+            this.txtOutput.TabIndex = 0;
+            //
+            // pictureBox1
+            //
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(652, 521);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "OpenCvSharpEx ShapeMatcher Sample";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnLoadPattern;
+        private System.Windows.Forms.Button btnTeach;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- src/OpenCvSharpEx.Sample/Form1.cs
+++ src/OpenCvSharpEx.Sample/Form1.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
@@ -16,63 +11,213 @@ namespace OpenCvSharpEx.Sample
 {
     public partial class Form1 : Form
     {
+        private ShapeMatcher shapeMatcher;
+        private Mat patternMat;
+        private Mat searchImageMat;
+        private ShapeMatcherResults lastSearchResult;
+
         public Form1()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            this.shapeMatcher = new ShapeMatcher() { MinAngle = 0, MaxAngle = 360, UseFusion = true, Refinement = RefinementMethod.Quadratic };
+            this.propertyGrid1.SelectedObject = this.shapeMatcher;
         }
-        ShapeMatcher shapeMatcher;
-        Mat pattern;
-        private void button1_Click(object sender, EventArgs e)
+
+        private void OnLoadPatternClick(object sender, EventArgs e)
         {
-            var patternFile = @"Q:\src\vision\Fastest_Image_Pattern_Matching\Test Images\20220611.bmp";
-            patternFile = @"Q:\src\vision\shape_based_matching\test\case1\train.png";
-            if (!File.Exists(patternFile))
-                patternFile = @"D:\src\vision\shape_based_matching\test\case1\train.png";
-            this.shapeMatcher = new ShapeMatcher() { MinAngle = 0, MaxAngle = 360 };
-            using (var pattern = Cv2.ImRead(patternFile))
+            // Q:\src\vision\shape_based_matching\test\case1\train.png
+            using (var ofd = new OpenFileDialog())
             {
-                var mat = new Mat(pattern, new Rect(50, 50, pattern.Cols - 100, pattern.Rows - 100));
+                ofd.Filter = "Image Files|*.bmp;*.png;*.jpg;*.jpeg|All files (*.*)|*.*";
+                if (ofd.ShowDialog() == DialogResult.OK)
                 {
-                    if (mat.Channels() > 1)
-                        Cv2.CvtColor(mat, mat, ColorConversionCodes.RGB2GRAY);
-                    this.shapeMatcher.Teach(mat);
-                    this.pattern = mat;
-                    this.Invalidate();
+                    this.patternMat?.Dispose();
+                    this.patternMat = new Mat(ofd.FileName, ImreadModes.Color);
+                    if (this.patternMat.Empty())
+                    {
+                        MessageBox.Show("Could not open or find the image");
+                        this.patternMat = null;
+                        return;
+                    }
+                    this.pictureBox1.Image?.Dispose();
+                    this.pictureBox1.Image = this.patternMat.ToBitmap();
+                    this.Log($"Pattern '{ofd.FileName}' loaded.");
                 }
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void OnTeachClick(object sender, EventArgs e)
         {
-            var imageFile = @"Q:\src\vision\Fastest_Image_Pattern_Matching\Test Images\Src1.bmp";
-            imageFile = @"Q:\src\vision\shape_based_matching\test\case1\test.png";
-            if (!File.Exists(imageFile))
-                imageFile = @"D:\src\vision\shape_based_matching\test\case1\test.png";
-            using (var image = Cv2.ImRead(imageFile))
+            if (this.patternMat == null)
             {
-                if (image.Channels() > 1)
-                    Cv2.CvtColor(image, image, ColorConversionCodes.BGR2GRAY);
+                MessageBox.Show("Please load a pattern image first.");
+                return;
+            }
+
+            using (var gray = new Mat())
+            {
+                if (this.patternMat.Channels() > 1)
+                    Cv2.CvtColor(this.patternMat, gray, ColorConversionCodes.BGR2GRAY);
+                else
+                    this.patternMat.CopyTo(gray);
+
+                this.Log("Teaching pattern...");
                 var sw = Stopwatch.StartNew();
-                this.shapeMatcher.UseFusion = true;
-                this.shapeMatcher.Refinement = RefinementMethod.Quadratic;
-                var r = this.shapeMatcher.Search(image);
-                Debug.WriteLine($"Time: {sw.ElapsedMilliseconds} ms");
+                this.shapeMatcher.Teach(gray);
+                sw.Stop();
+                this.Log($"Teaching complete in {sw.ElapsedMilliseconds} ms.");
+
+                this.DrawFeatures();
+            }
+        }
+
+        private void OnLoadImageClick(object sender, EventArgs e)
+        {
+            // Q:\src\vision\shape_based_matching\test\case1\test.png
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files|*.bmp;*.png;*.jpg;*.jpeg|All files (*.*)|*.*";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    this.searchImageMat?.Dispose();
+                    this.searchImageMat = new Mat(ofd.FileName, ImreadModes.Color);
+                    if (this.searchImageMat.Empty())
+                    {
+                        MessageBox.Show("Could not open or find the image");
+                        this.searchImageMat = null;
+                        return;
+                    }
+                    this.pictureBox1.Image?.Dispose();
+                    this.pictureBox1.Image = this.searchImageMat.ToBitmap();
+                    this.lastSearchResult = null;
+                    this.Log($"Search image '{ofd.FileName}' loaded.");
+                }
+            }
+        }
+
+        private void OnSearchClick(object sender, EventArgs e)
+        {
+            if (this.shapeMatcher == null)
+            {
+                MessageBox.Show("Matcher not initialized. Please teach a pattern first.");
+                return;
+            }
+            if (this.searchImageMat == null)
+            {
+                MessageBox.Show("Please load an image to search in.");
+                return;
             }
 
-        }
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            if (this.pattern != null)
+            using (var gray = new Mat())
             {
-                using (var b = this.pattern.ToBitmap())
+                if (this.searchImageMat.Channels() > 1)
+                    Cv2.CvtColor(this.searchImageMat, gray, ColorConversionCodes.BGR2GRAY);
+                else
+                    this.searchImageMat.CopyTo(gray);
+
+                this.Log("Searching...");
+                var sw = Stopwatch.StartNew();
+                try
                 {
-                    var templateInfo = this.shapeMatcher.GetTemplate(0);
-                    var features = templateInfo.Features;
-                    var g = e.Graphics;
-                    g.DrawImage(b, 0, 0);
-                    foreach (var f in features)
-                        g.DrawEllipse(Pens.Lime, f.x - 3, f.y - 3, 6, 6);
+                    this.lastSearchResult = this.shapeMatcher.Search(gray);
                 }
+                catch (OpenCvSharpException ex)
+                {
+                    this.Log($"Error during search: {ex.Message}");
+                    if (ex.Message.Contains("No pattern is taught yet"))
+                    {
+                        MessageBox.Show("Please teach a pattern before searching.");
+                    }
+                    return;
+                }
+                sw.Stop();
+
+                if (this.lastSearchResult != null && this.lastSearchResult.Score > 0)
+                {
+                    this.Log($"Search complete in {sw.ElapsedMilliseconds} ms.");
+                    this.Log($"Result:");
+                    this.Log($"  Score: {this.lastSearchResult.Score:F2}");
+                    this.Log($"  Angle: {this.lastSearchResult.Angle:F2}°");
+                    this.Log($"  Location: ({this.lastSearchResult.Location.X:F2}, {this.lastSearchResult.Location.Y:F2})");
+
+                    this.DrawSearchResult();
+                }
+                else
+                {
+                    this.Log($"Search complete in {sw.ElapsedMilliseconds} ms. No match found.");
+                    this.pictureBox1.Image?.Dispose();
+                    this.pictureBox1.Image = this.searchImageMat.ToBitmap();
+                }
+            }
+        }
+
+        private void Log(string message)
+        {
+            if (this.txtOutput.InvokeRequired)
+            {
+                this.txtOutput.Invoke(new Action(() => this.Log(message)));
+            }
+            else
+            {
+                this.txtOutput.AppendText(message + Environment.NewLine);
+            }
+        }
+
+        private void DrawFeatures()
+        {
+            if (this.patternMat == null || this.shapeMatcher == null) return;
+
+            var templateInfo = this.shapeMatcher.GetTemplate(0);
+            if (templateInfo == null || templateInfo.Features == null) return;
+
+            var features = templateInfo.Features;
+            var bmp = this.patternMat.ToBitmap();
+            using (var g = Graphics.FromImage(bmp))
+            {
+                var offset = this.shapeMatcher.GetPatternOffset();
+                foreach (var f in features)
+                    g.DrawEllipse(Pens.Lime, f.x - offset.X - 1, f.y - offset.Y - 1, 3, 3);
+            }
+            this.pictureBox1.Image?.Dispose();
+            this.pictureBox1.Image = bmp;
+        }
+
+        private void DrawSearchResult()
+        {
+            if (this.searchImageMat == null || this.lastSearchResult == null) return;
+
+            using (var resultMat = new Mat())
+            {
+                if (this.searchImageMat.Channels() == 1)
+                    Cv2.CvtColor(this.searchImageMat, resultMat, ColorConversionCodes.GRAY2BGR);
+                else
+                    this.searchImageMat.CopyTo(resultMat);
+
+                var points = this.lastSearchResult.RotatedBounds.Points().Select(p => new OpenCvSharp.Point(p.X, p.Y)).ToArray();
+                Cv2.Polylines(resultMat, new[] { points }, true, Scalar.LimeGreen, 2);
+
+                var center = this.lastSearchResult.Location;
+                Cv2.DrawMarker(resultMat, new OpenCvSharp.Point(center.X, center.Y), Scalar.Red, MarkerTypes.Cross, 10, 2);
+
+                this.pictureBox1.Image?.Dispose();
+                this.pictureBox1.Image = resultMat.ToBitmap();
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            this.shapeMatcher?.Dispose();
+            this.patternMat?.Dispose();
+            this.searchImageMat?.Dispose();
+            this.pictureBox1.Image?.Dispose();
+        }
+
+        private void OnPropertyValueChanged(object s, PropertyValueChangedEventArgs e)
+        {
+            if (this.shapeMatcher != null)
+            {
+                this.Log($"Matcher property changed: {e.ChangedItem.Label} = {e.ChangedItem.Value}");
             }
         }
     }
